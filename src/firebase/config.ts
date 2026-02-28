db4357041d6437770dfefc9421ecd7f8d1142a53
Ishini Shanika <ishinishanika2003@gmail.com>
2026-02-28 08:54:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5304618315-4b77f",
+  "appId": "1:415469247103:web:807a28440126c65bc77d4a",
+  "apiKey": "AIzaSyAGd4QwFykL6l0vz3N2ajCZDz5qBR08CFg",
+  "authDomain": "studio-5304618315-4b77f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "415469247103"
+};
