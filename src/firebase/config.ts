a1d29d213112b48d557b782ba1532f0bd80cace0
Ishini Shanika <ishinishanika2003@gmail.com>
2026-02-28 11:44:21 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:415469247103:web:807a28440126c65bc77d4a",
   "apiKey": "AIzaSyAGd4QwFykL6l0vz3N2ajCZDz5qBR08CFg",
   "authDomain": "studio-5304618315-4b77f.firebaseapp.com",
+  "storageBucket": "studio-5304618315-4b77f.appspot.com",
   "measurementId": "",
   "messagingSenderId": "415469247103"
 };
